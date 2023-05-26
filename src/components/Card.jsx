701acc7125516efeb5dbb33d29281237f9632b05
nolonooojx23 <119511297+nolonooojx23@users.noreply.github.com>
2023-05-26 16:54:33 +0300

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,14 @@ import { useTranslation } from 'react-i18next';
 function Card({ name, img }) {
 	const { t } = useTranslation(["profile"]);
 	return (
-		<div className="service-card bg-white rounded-[10px] w-[250px] h-[250px] max-md:w-[230px] max-md:h-[230px] flex flex-col justify-center items-center relative" onClick={() => {
+		<div className="service-card bg-white rounded-[10px] w-[250px] h-[250px] max-md:w-[200px] max-md:h-[200px] max-[430px]:w-[150px] max-[430px]:h-[150px] flex flex-col justify-center items-center relative max-[430px]:mt-5" onClick={() => {
 			document.documentElement.scrollTop = 0
 		}}>
 			<div className="service-card-info z-[3] flex flex-col justify-center items-center">
-				<div className="service-card-img mb-5 ">
-					<img src={img} alt="" />
+				<div className="service-card-img mb-5 max-md:w-[50px] object-contain ">
+					<img  src={img} alt="" />
 				</div>
-				<h1 className='mt-4 text-lg text-center px-3 font-semibold '>{t(name)}</h1>
+				<h1 className='mt-4 text-lg text-center px-3 font-semibold max-md:text-sm max-[430px]:text-[12px] max-[430px]:mt-1 '>{t(name)}</h1>
 			</div>
 		</div>
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,7 +20,7 @@ function Services({ services }) {
 
   return (
     <Element name="servicesSection">
-      <div className='services w-full px-16 py-16 bg-[#d5def8] relative flex flex-col justify-center items-center max-md:px-10 max-sm:px-5'>
+      <div className='services w-full px-16 py-16 bg-[#d5def8] relative flex flex-col justify-center items-center max-md:px-5 max-sm:px-2'>
         <div className="w-full flex justify-between items-center mt-6 flex-wrap max-md:justify-center gap-10">
           <h1 className='text-center text-4xl font-semibold max-sm:text-3xl '>{t('Перечень наших услуг:')}</h1>
           <div className="flex items-center bg-white px-4 rounded-lg border-[grey] border">
@@ -34,7 +34,7 @@ function Services({ services }) {
             />
           </div>
         </div>
-        <div className="services-cards flex justify-around items-center mt-10 py-10 flex-wrap gap-10 max-h-[100vh] overflow-y-scroll px-10">
+        <div className="services-cards flex justify-around items-center mt-10 py-10 flex-wrap gap-10 max-md:gap-7 max-[430px]:gap-3 max-h-[100vh] overflow-y-scroll px-10 max-md:px-2 ">
           {filteredServices.length > 0 ? (
             filteredServices.map((item) => (
               <div key={item.id}>
