8a92d03e2dfa6e4b14fae63fdeac3d562ed5f908
nolonooojx23 <119511297+nolonooojx23@users.noreply.github.com>
2023-06-16 18:42:24 +0300

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,7 @@ function Home() {
 			className='home overflow-hidden w-full pt-20'>
 			<div className="home-header w-full flex justify-between items-center gap-10 py-10 px-16 max-lg:flex-wrap max-lg:justify-center max-lg:px-10 max-sm:px-5 max-[430px]:px-2">
 				<motion.div className="home-header-text w-1/2 max-lg:w-full" variants={HomeAnimationFirst}>
-					<h1 className='text-[42px] leading-[1] font-semibold max-lg:text-3xl max-md:text-4xl max-sm:text-3xl max-[430px]:hidden
+					<h1 className='text-[40px] leading-[1] font-semibold max-lg:text-3xl max-md:text-4xl max-sm:text-3xl max-[430px]:hidden
 					'>{t("Организуем рекламу вашей компании")}<span className='text-[#2751cd]'> {t("по всему Узбекистану!")}</span></h1>
 					<p className='max-[430px]:hidden mt-10 text-xl max-lg:text-base max-md:text-lg max-sm:text-base max-[430px]:text-sm max-[430px]:text-center max-sm:mt-4 max-[430px]:mt-5'>{t("Мы тесно сотрудничаем, чтобы ваши объявления, которые вам больше всего нужны для вашей компании, стали достоянием общественности. С уникальной стратегией каждого пункта назначения!")}</p>
 					<p className='max-[430px]:hidden mt-10 text-xl max-lg:text-base max-md:text-lg max-sm:text-base max-[430px]:text-sm max-[430px]:text-center max-sm:mt-4 max-[430px]:mt-5'>{t("Наше рекламное агентство предлагает вам 18 различных услуг по всей территории Узбекистана!")}</p>
